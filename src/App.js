c36b7ae9de839bd5570138a6ca9eec3b01a8bc6d
marieneoo <149239439+marieneoo@users.noreply.github.com>
2024-02-06 05:01:10 -0800

    valentine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [NoCount, setNoCount] = useState(0);
+  const [YesPressed, setYesPressed] = useState(false);
+  const ButtonSize = NoCount*20+16
+  
+  function handleNoClick() {
+    setNoCount(NoCount+1)
+  }
+
+  function getPhrases() {
+    return(
+      phrases[Math.min(NoCount,phrases.length-1)]
+    )
+  }
+
+  const phrases = [
+    "Ոչ",
+    "Վստա՞հ ես",
+    "Հաստատ վստա՞հ ես",
+    "Շաատ եմ խնդրում",
+    "Մի վարվիր էսպես",
+    "Հեսա կլացեմ",
+    "Դու կոտրում ես իմ սիրտը"
+  ]
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        {YesPressed ? (
+          <div className="yes_pressed">
+            <img className="yes_pressed_pic" src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"></img>
+            <h3>Դզեցցց!!!</h3>
+          </div>
+        ):(
+          <div className="no_pressed">
+            <img className="no_pressed_pic" src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"></img>
+            <h2>Կլինե՞ս իմ վալենտինը</h2>
+
+            <div className="buttons">
+            <button className="yes_button" style={{fontSize: ButtonSize}} 
+            onClick={()=>setYesPressed(true)}>Այո</button>
+
+            <button className="no_button" onClick={handleNoClick}>{getPhrases()}</button>
+            </div>
+          </div>
+        )}
+
+      </div>
   );
 }
 

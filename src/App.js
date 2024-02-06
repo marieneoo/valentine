df00b365aa288ece3f2eba71a566709dbc993ab0
marieneoo <149239439+marieneoo@users.noreply.github.com>
2024-02-06 05:34:11 -0800

    comments

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [NoCount, setNoCount] = useState(0);
-  const [YesPressed, setYesPressed] = useState(false);
-  const ButtonSize = NoCount * 10 + 16;
+  const [NoCount, setNoCount] = useState(0); //stexcum enq state no-i button- click anelu qanaky hashvelu hamar
+  const [YesPressed, setYesPressed] = useState(false); // stexcum enq state, vor henc yes- vra sxmenq value-n poxvi true u ejy poxenq
+  const ButtonSize = NoCount * 10 + 16; // button-i size-i voroshum, no-i button-i vra click aracneri het matematikakan hashvark
 
   function handleNoClick() {
     setNoCount(NoCount + 1);
-  }
+  } // funkcian stexcvac a no-i clickeri qanaky avelacnelu hamar
 
   function getPhrases() {
     return phrases[Math.min(NoCount, phrases.length - 1)];
-  }
+  } // tpum e phrase-eri no count-erord index-i tak gtnvox arjeqy erb vor hasnum e verjin tpum a anyndhat amenaverjin popoxakany
 
   const phrases = [
     "Ոչ",
@@ -45,8 +45,8 @@ function App() {
           <div className="buttons">
             <button
               className="yes_button"
-              style={{ fontSize: ButtonSize }}
-              onClick={() => setYesPressed(true)}
+              style={{ fontSize: ButtonSize }} // font size-y talis enq buttonsize-i arjeqy
+              onClick={() => setYesPressed(true)} // mer state-y,vori anuny yespressed-a poxum enq true
             >
               Այո
             </button>
